52da03b37000e29c296296d8cba4d0f747685fa6
Dan Shumayev <dan.shumayev@mail.huji.ac.il>
2021-05-31 21:18:51 +0300

    Trying to design the Scheduler, still stuck. Not sure about the right initialization, etc.

--- Project-Ex2/Scheduler.cpp
+++ Project-Ex2/Scheduler.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Scheduler.h"
+
+
+Scheduler::Scheduler(int quantum_usecs)
+: thread_quantum_(quantum_usecs), // TODO - int to size_t - not dangerous?
+    min_available_tid_(1)
+    // TODO - What else should be initialized?
+{}
+
+void Scheduler::mask_signal(const int signo)
+{
+    if (sigaddset(&blocked_signals_, signo)) /** TODO - is it okay to pass signo to it?
+                                                    sigaddset() receives non-const int */
+    {
+        throw std::domain_error(MASK_SIGNAL_ERR);
+    }
+    else if (sigprocmask(SIG_BLOCK, &blocked_signals_, nullptr))
+    {
+        throw std::domain_error(MASK_SIGNAL_ERR);
+    }
+}
+
+void Scheduler::unmask_signals()
+{
+    if (sigprocmask(SIG_UNBLOCK, &blocked_signals_, nullptr))
+    {
+        throw std::domain_error(UNMASK_SIGNAL_ERR);
+    }
+}
--- Project-Ex2/Scheduler.h
+++ Project-Ex2/Scheduler.h
@@ -10,8 +10,13 @@
 #include <list> // std::list
 #include <map> // std::map
 #include <sys/time.h> // itimerval
+#include <stdexcept> // std::domain_error
 
 typedef std::shared_ptr<Thread> = threadSharedPtr;
+
+/** Error messages: */
+const std::string MASK_SIGNAL_ERR ("sigsetadd()/sigprocmask() failed");
+const std::string UNMASK_SIGNAL_ERR ("sigprocmask() failed");
 
 /**
  * Round-Robin scheduler for user-level threads.
@@ -25,6 +30,7 @@ class Scheduler {
     std::map<size_t, threadSharedPtr> tid_to_thread_map_;
     // TODO - blocked threads (by another thread/mutex) structures?
         /** Accounting information: */
+    size_t thread_quantum_;
     size_t total_quantum_;
     size_t min_available_tid_; /** TODO - What if there's more than one available ID
                                     between running threads? Should we maintain some
@@ -32,9 +38,23 @@ class Scheduler {
         /** Signal-handling: */
     struct itimerval virtual_timer_; // Dedicated to time the running thread's execution
     struct sigaction signal_handler_;
+    sigset_t blocked_signals_;
 
 public:
     // TODO - How to construct this class? What's the purpose?
+
+    Scheduler(int quantum_usecs);
+
+    /**
+     * Adds the signal with signo number into the masked signals of the running thread
+     * @param signo The signal number to be masked
+     */
+    void mask_signal(const int signo);
+
+    /**
+     * Unmask all masked signals in the current running thread
+     */
+    void unmask_signals();
 };
 
 
