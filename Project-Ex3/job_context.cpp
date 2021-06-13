0b1e18b4d53e85378959195da221e8fcdc300ed0
Dan Shumayev <dan.shumayev@mail.huji.ac.il>
2021-06-13 15:57:51 +0300

    Embedded fixes and modifications after Kerbel's remarks. Follow the TODOs to continue implementing.

--- Project-Ex3/job_context.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by dan-os on 09/06/2021.
-//
-
-#include "job_context.h"
---
+++ Project-Ex3/thread_context.cpp
@@ -0,0 +1,34 @@
+//
+// Created by dan-os on 12/06/2021.
+//
+
+#include <algorithm>
+#include "thread_context.h"
+
+void ThreadContext::invokeMapPhase()
+{
+    JobContext& currJobContext = *currentJobContext_;
+    size_t ix;
+    while ((ix = currJobContext.lastThreadAtomicGetIncrement()) < currJobContext.getNumOfInputElems())
+    {
+        auto elem = currJobContext.getInputVector()[ix];
+        currJobContext.invokeClientMapRoutine(elem.first, elem.second, static_cast<void *>(this));
+    }
+    // TODO percentage, job state, etc. accordingly - to be continued...
+}
+
+void ThreadContext::invokeSortPhase()
+{
+    std::sort(intermediateVec_.begin(), intermediateVec_.end());
+    // TODO - ensure we don't need nothing else here
+}
+
+void ThreadContext::invokeShufflePhase()
+{
+    // TODO - implement logic
+}
+
+void ThreadContext::invokeReducePhase()
+{
+    // TODO - implement logic
+}
