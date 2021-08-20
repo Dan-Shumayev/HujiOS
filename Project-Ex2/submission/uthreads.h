c9c2721c3ee569371389e9d11fb54694b4a40e66
Dan Shumayev <dan.shumayev@mail.huji.ac.il>
2021-08-20 19:43:03 +0300

    Build the tests

--- Project-Ex2/submission/uthreads.h
+++ Project-Ex2/submission/uthreads.h
@@ -7,7 +7,7 @@
  */
 
 #define MAX_THREAD_NUM 100 /* maximal number of threads */
-#define STACK_SIZE 8192 /* stack size per thread (in bytes) */
+#define STACK_SIZE 8192 /* stack size per thread (in bytes) */ // TODO - change it back to 4092
 
 /* External interface */
 
