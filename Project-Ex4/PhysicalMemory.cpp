601b05158dcf147e1d966381cd0dd149b85cfc5a
Dan Shumayev <dan.shumayev@mail.huji.ac.il>
2022-05-24 17:20:16 +0300

    Restore the original MemoryConstans.h and PhysicalMemory.cpp to fit the compilation process required; In order to run Kerbel's tests, refer to its respective test repository.

--- Project-Ex4/PhysicalMemory.cpp
+++ Project-Ex4/PhysicalMemory.cpp
@@ -1,78 +1,58 @@
 #include "PhysicalMemory.h"
-#include "MemoryConstants.h"
-
-
 #include <vector>
 #include <unordered_map>
 #include <cassert>
 #include <cstdio>
 
-
-#ifdef INC_TESTING_CODE
-std::unique_ptr<std::stringstream> Trace::ss (new std::stringstream());
-#endif
-
-
 typedef std::vector<word_t> page_t;
 
 std::vector<page_t> RAM;
-
-/** Maps virtual addresses to their page table on disk. */
 std::unordered_map<uint64_t, page_t> swapFile;
 
 void initialize() {
     RAM.resize(NUM_FRAMES, page_t(PAGE_SIZE));
 }
 
-
 void PMread(uint64_t physicalAddress, word_t* value) {
-    if (RAM.empty())
+    if (RAM.empty()) {
         initialize();
+    }
 
     assert(physicalAddress < RAM_SIZE);
 
-    *value = RAM[physicalAddress / PAGE_SIZE][physicalAddress % PAGE_SIZE];
-
-#ifdef INC_TESTING_CODE
-    Trace::stream() << "PMread(" << physicalAddress << ") = " << *value << std::endl;
-#endif
+    uint64_t frameIndex = physicalAddress / PAGE_SIZE;
+    uint64_t frameOffset = physicalAddress % PAGE_SIZE;
+    *value = RAM[frameIndex][frameOffset];
  }
 
 void PMwrite(uint64_t physicalAddress, word_t value) {
-#ifdef INC_TESTING_CODE
-    Trace::stream() << "PMwrite(" << physicalAddress << ", " << value << ")" << std::endl;
-#endif
-
-    if (RAM.empty())
+    if (RAM.empty()) {
         initialize();
+    }
 
     assert(physicalAddress < RAM_SIZE);
 
-    RAM[physicalAddress / PAGE_SIZE][physicalAddress % PAGE_SIZE] = value;
+    uint64_t frameIndex = physicalAddress / PAGE_SIZE;
+    uint64_t frameOffset = physicalAddress % PAGE_SIZE;
+    RAM[frameIndex][frameOffset] = value;
 }
 
 void PMevict(uint64_t frameIndex, uint64_t evictedPageIndex) {
-#ifdef INC_TESTING_CODE
-    Trace::stream() << "PMevict(" << frameIndex << ", " << evictedPageIndex << ")" << std::endl;
-#endif
-
-    if (RAM.empty())
+    if (RAM.empty()) {
         initialize();
+    }
 
-    assert(swapFile.find(evictedPageIndex) == swapFile.end());
     assert(frameIndex < NUM_FRAMES);
     assert(evictedPageIndex < NUM_PAGES);
+    assert(swapFile.find(evictedPageIndex) == swapFile.end());
 
     swapFile[evictedPageIndex] = RAM[frameIndex];
 }
 
 void PMrestore(uint64_t frameIndex, uint64_t restoredPageIndex) {
-#ifdef INC_TESTING_CODE
-    Trace::stream() << "PMrestore(" << frameIndex << ", " << restoredPageIndex << ")" << std::endl;
-#endif
-
-    if (RAM.empty())
+    if (RAM.empty()) {
         initialize();
+    }
 
     assert(frameIndex < NUM_FRAMES);
 
@@ -86,3 +66,4 @@ void PMrestore(uint64_t frameIndex, uint64_t restoredPageIndex) {
     RAM[frameIndex] = std::move(swapFile[restoredPageIndex]);
     swapFile.erase(restoredPageIndex);
 }
+
