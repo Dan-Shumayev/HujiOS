bbc34d1865e9de287862dbb6dcf5144add96fac9
Dan Shumayev <dan.shumayev@mail.huji.ac.il>
2022-06-11 03:08:28 +0300

    Add a test for the process limitation.

--- Project-Ex5/tests/test_containers.py
+++ Project-Ex5/tests/test_containers.py
@@ -2,15 +2,15 @@ import subprocess
 from pathlib import Path
 import sys
 
-CONTAINER_EXE = Path(__file__).parent.parent / "build" / "container"
+CONTAINER_EXE = Path(__file__).parent.parent / "build" / "container"  # {$PROJECT_ROOT_DIR}/build/container
 
-IMAGE_DIR = Path(__file__).parent.parent / "image"
+IMAGE_DIR = Path(__file__).parent.parent / "image"  # {$PROJECT_ROOT_DIR}/image
 
-if(not CONTAINER_EXE.exists()):
+if not CONTAINER_EXE.exists():
     print(f"Expected container executable at path {CONTAINER_EXE}", file=sys.stderr)
     sys.exit(1)
 
-if(not IMAGE_DIR.exists()):
+if not IMAGE_DIR.exists():
     print(f"Expected image directory at {IMAGE_DIR}", file=sys.stderr)
     sys.exit(1)
 
@@ -25,9 +25,10 @@ def run_container(
     args = [str(CONTAINER_EXE), new_hostname, str(
         image_dir), str(num_proc), str(program_path)] + program_args
     print("args", args)
-    res = subprocess.run(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    print(res)
+    res = subprocess.run(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         universal_newlines=True)
     return res
+
 
 def test_hostname():
     res = run_container(new_hostname="testHostName", image_dir=IMAGE_DIR,
@@ -43,4 +44,11 @@ def test_program_args():
     assert res.returncode == 0
     assert "GNU bash, version" in res.stdout
 
+def test_max_processes():
+    args = [str(CONTAINER_EXE), "testHostName", str(IMAGE_DIR), str(2), str(Path("/bin/bash"))]
 
+    res = subprocess.run(args, universal_newlines=True, text=True, input="sleep 3 | sleep 3",
+                         capture_output=True)
+
+    assert res.returncode == 0
+    assert "No child processes" in res.stderr
